174b2360b7801bb2fc08835ffbd1c019a6879be6
Mukhamedzhan Nurmukhamed <47817099+mukosha@users.noreply.github.com>
2021-11-12 16:08:24 +0600

    Add files via upload

---
+++ train_fk.m
@@ -0,0 +1,43 @@
+data_size = 200000;
+[XTrain, YTrain] = create_dataset_fk(data_size);
+
+XTrain = reshape(XTrain', [1, 1, size(XTrain,2),size(XTrain,1)]);
+
+
+nFeatures = 5;
+numClasses = 3;
+
+layers = [ ...
+    imageInputLayer([1 1 nFeatures])
+    fullyConnectedLayer(2048)
+    leakyReluLayer
+    fullyConnectedLayer(1024)
+    leakyReluLayer
+    fullyConnectedLayer(512)
+    leakyReluLayer
+    fullyConnectedLayer(256)
+    leakyReluLayer
+    fullyConnectedLayer(128)
+    reluLayer
+    fullyConnectedLayer(numClasses)  
+    regressionLayer  
+    ]
+
+maxEpochs = 25;
+miniBatchSize = 250;
+
+options = trainingOptions('adam', ...
+    'ExecutionEnvironment','cpu', ...
+    'GradientThreshold',1, ...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'SequenceLength','longest', ...
+    'Shuffle','never', ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+
+
+net_fk = trainNetwork(XTrain, YTrain,layers,options);
+
+
+save net_fk
